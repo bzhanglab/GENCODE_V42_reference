09b4d3b67dea02c6c8fa9a8b2906cf4165cfe43b
douyc <douyongchao@gmail.com>
2022-12-05 07:50:10 -0600

    Update creat_workspace_and_download_reference.sh

--- creat_workspace_and_download_reference.sh
+++ creat_workspace_and_download_reference.sh
@@ -1,6 +1,6 @@
 mkdir genome
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/GRCh38.primary_assembly.genome.fa.gz
-mv RCh38.primary_assembly.genome.fa.gz genome/
+mv GRCh38.primary_assembly.genome.fa.gz genome/
 
 mkdir annotation
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/gencode.v42.basic.annotation.gtf.gz
