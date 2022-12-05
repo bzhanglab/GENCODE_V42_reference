67cbac1ec37001d2fdcec9e69b3f0e6997070b3a
douyc <douyongchao@gmail.com>
2022-12-05 11:50:20 -0600

    Update prepare_protein_database.pl

--- prepare_protein_database.pl
+++ prepare_protein_database.pl
@@ -10,16 +10,23 @@ use Env qw(@PATH);
 
 @PATH = ($FindBin::RealBin, @PATH);
 
-$work_dir="$PATH[0]/gistic_reference";
+$work_dir="$PATH[0]/protein_database";
 $script_dir="$PATH[0]/scripts/";
 $current_dir=$PATH[0];
 
-#######prepare rg file
-`perl $script_dir/generate-gencode-rg-no-chrm.pl $work_dir $current_dir`;
+#######filter proteins in basic chr annotation
+#`perl $script_dir/filter_basic_protein.pl $work_dir $current_dir`;
 
-#######sort rg file
-`Rscript $script_dir/reformat-rg.r $work_dir`;
+#######get protein group
+#`perl $script_dir/protein_group.pl $work_dir`;
 
-####################
-`cp $script_dir/main_convert.m $work_dir/main_convert.m`;
+#######fitler protein by our method
+#`Rscript $script_dir/filter-protein.r $work_dir  $current_dir`;
+
+#######filter contaminants and reformat contaminant database
+#`perl $script_dir/filter_contaminants_and_reformat_database.pl $work_dir`;
+
+#######reformat protein database and add contaminants
+`perl $script_dir/to-UniProt-format.pl $work_dir`;
+`perl $script_dir/to-UniProt-format-ig-tr-nmd-lof.pl $work_dir`;
 
